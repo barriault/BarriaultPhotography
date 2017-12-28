071d13ccf69185d0eb8d60b17719d11814021779
Jeff Barriault <jeff@barriault.net>
2017-12-28 15:22:35 -0600

    fix problems with smugmug api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,11 @@ class WelcomeController < ApplicationController
   
   def index
     
-    my_nick = ENV['SMUGMUG_NICKNAME']
-    my_pass = ENV['SMUGMUG_PASSWORD']
-    smug = Smile.auth( my_nick, my_pass )
-    album = Smile::Album.find(:album_id => "21511473", :album_key => "L25jTF")
-    @photos = album.photos
+    # my_nick = ENV['SMUGMUG_NICKNAME']
+    # my_pass = ENV['SMUGMUG_PASSWORD']
+    # smug = Smile.auth( my_nick, my_pass )
+    # album = Smile::Album.find(:album_id => "21511473", :album_key => "L25jTF")
+    # @photos = album.photos
 
     respond_to do |format|
       format.html # index.html.erb
